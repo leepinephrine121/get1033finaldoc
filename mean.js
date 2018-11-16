031ebc9921eeb76deec04c88a0746044a4eab72b
leepinephrine121 <43434279+leepinephrine121@users.noreply.github.com>
2018-11-16 13:39:23 +0800

    Add files via upload

---
+++ mean.js
@@ -0,0 +1,201 @@
+var database = [
+	{"album": "J-neration 1", "artist": "anubasu-anubasu", "soundtrack": "Shocking", "site": "https://youtu.be/BV8xai-4loI"},
+	{"album": "J-neration 2", "artist": "anubasu-anubasu", "soundtrack": "Screech Freaks", "site": "https://j-neration.bandcamp.com/track/screech-freaks"},
+	{"album": "J-neration 3", "artist": "anubasu-anubasu", "soundtrack": "Epsilon", "site": "https://youtu.be/WDwtunQRKUI"},
+	{"album": "J-neration 4", "artist": "anubasu-anubasu", "soundtrack": "Arabian Fighter", "site": "https://youtu.be/yjlTfqS5z8o"},
+	{"album": "J-neration 5", "artist": "anubasu-anubasu", "soundtrack": "D!sco W3ap0n", "site": "https://youtu.be/fkCKMcIPm5M"},
+	{"album": "J-neration Gap 1", "artist": "anubasu-anubasu", "soundtrack": "T.I.H.D. (This Is Hard Drop)", "site": "https://youtu.be/SfFlSI9h9UA"},
+	{"album": "J-neration 1", "artist": "chill", "soundtrack": "Do It Right", "site": "https://youtu.be/wbEazFz-7B0"},
+	{"album": "J-neration 2", "artist": "chill", "soundtrack": "Eorthe", "site": "https://youtu.be/oam3TuSubQo"},
+	{"album": "J-neration 3", "artist": "chill", "soundtrack": "By Your Side", "site": "https://youtu.be/kZBPylIJV10"},
+	{"album": "J-neration Gap 2", "artist": "cobalt green", "soundtrack": "Flashback", "site": "https://j-neration.bandcamp.com/track/flashback"},
+	{"album": "J-neration 1", "artist": "DecisionS kanone + nora2r", "soundtrack": "Frivolous Love", "site": "https://youtu.be/SuL_0oJaoek"},
+	{"album": "J-neration 1", "artist": "DJ NECOJITA", "soundtrack": "Lucky Eyes", "site": "https://youtu.be/NM2aIYJYo9s"},
+	{"album": "J-neration 2", "artist": "DJ NECOJITA", "soundtrack": "Black Box", "site": "https://youtu.be/UsQBE9QPiBU"},
+	{"album": "J-neration 3", "artist": "DJ NECOJITA", "soundtrack": "Caramel Soundwave", "site": "https://youtu.be/nUanb4S2f1c"},
+	{"album": "J-neration 4", "artist": "DJ NECOJITA", "soundtrack": "Subliminial Incarnate", "site": "https://youtu.be/WDZkj9m9s6w"},
+	{"album": "J-neration 6", "artist": "DJ NECOJITA", "soundtrack": "Salty Hyperlaser", "site": "https://youtu.be/E3lwEmBHOY8"},
+	{"album": "J-neration 7", "artist": "DJ NECOJITA", "soundtrack": "Heavenly Cookie", "site": "https://j-neration.bandcamp.com/track/heavenly-cookie"},
+	{"album": "J-neration Gap 1", "artist": "DJ NECOJITA", "soundtrack": "Pandora Battery", "site": "https://youtu.be/6pPiJkUxpII"},
+	{"album": "J-neration Gap 2", "artist": "DJ NECOJITA", "soundtrack": "Noise Maker", "site": "https://j-neration.bandcamp.com/track/noize-maker"},
+	{"album": "J-neration 1", "artist": "ETIA", "soundtrack": "Killer Bee", "site": "https://youtu.be/yDd8j6M7qek"},
+	{"album": "J-neration 3", "artist": "Vivid Frog", "soundtrack": "Vivid Frog", "site": "https://youtu.be/7pBpR9oazec"},
+	{"album": "J-neration 1", "artist": "Freezer", "soundtrack": "Lost Sign feat. mippai", "site": "https://youtu.be/1_vuhPZ6RW4"},
+	{"album": "J-neration 2", "artist": "Freezer", "soundtrack": "Lemonade feat. mippai", "site": "https://youtu.be/RNZdt3EYc0w"},
+	{"album": "J-neration 3", "artist": "Freezer", "soundtrack": "collapse feat. mippai", "site": "https://youtu.be/MpCuy7lrP5w"},
+	{"album": "J-neration 4", "artist": "Freezer", "soundtrack": "Falling Down", "site": "https://youtu.be/YHKIB25xFZc"},
+	{"album": "J-neration 5", "artist": "Freezer", "soundtrack": "Vine Thicket", "site": "https://youtu.be/TBDp8trWGs0"},
+	{"album": "J-neration 6", "artist": "Freezer", "soundtrack": "Candy Princess feat. 妃苺", "site": "https://youtu.be/NE21qA-G70s"},
+	{"album": "J-neration 7", "artist": "Freezer", "soundtrack": "Marshmallow Pop Star feat. 妃苺", "site": "https://youtu.be/SAK1qcDTiT4"},
+	{"album": "J-neration 8", "artist": "Freezer", "soundtrack": "Pink Elephant feat. 妃苺", "site": "https://youtu.be/bWjp2vghG0A"},
+	{"album": "J-neration 9", "artist": "Freezer", "soundtrack": "Jellyfish feat. 紀苺", "site": "https://youtu.be/dkJ5dUX-tdk"},
+	{"album": "J-neration Gap 1", "artist": "Freezer", "soundtrack": "F**kin' Life", "site": "https://youtu.be/rTdSDKGqLYo"},
+	{"album": "J-neration Gap 2", "artist": "Freezer", "soundtrack": "Wendigo", "site": "https://j-neration.bandcamp.com/track/wendigo"},
+	{"album": "J-neration 4", "artist": "Getty", "soundtrack": "Jumper", "site": "https://youtu.be/5ane6zA0w5I"},
+	{"album": "J-neration 5", "artist": "Getty", "soundtrack": "Overfront", "site": "https://youtu.be/R0jLLnr7734"},
+	{"album": "J-neration 6", "artist": "Getty", "soundtrack": "Break It", "site": "https://youtu.be/IYAh6ugD_gk"},
+	{"album": "J-neration 7", "artist": "Getty", "soundtrack": "Growler", "site": "https://youtu.be/TE8sSW6EUu8"},
+	{"album": "J-neration 8", "artist": "Getty", "soundtrack": "Keep On Raving", "site": "https://j-neration.bandcamp.com/track/keep-on-raving"},
+	{"album": "J-neration 9", "artist": "Getty", "soundtrack": "Louder", "site": "https://youtu.be/mfin_e6i990"},
+	{"album": "J-neration Gap 1", "artist": "Getty", "soundtrack": "Pump It", "site": "https://youtu.be/q9HNmysUrtY"},
+	{"album": "J-neration Gap 2", "artist": "Getty", "soundtrack": "Party Out", "site": "https://youtu.be/Ga2jse8WZ0g"},
+	{"album": "J-neration 1", "artist": "gya9", "soundtrack": "promise", "site": "https://youtu.be/mex4cjo4vNI"},
+	{"album": "J-neration 2", "artist": "gya9 vs IVV", "soundtrack": "Dazzle", "site": "https://youtu.be/KsDVblrW0vE"},
+	{"album": "J-neration 3", "artist": "gya9", "soundtrack": "Nirai Kanai", "site": "https://youtu.be/PGMet2V16So"},
+	{"album": "J-neration 4", "artist": "gya9", "soundtrack": "Vengeful Spirit", "site": "https://youtu.be/AvW3OGHttjM"},
+	{"album": "J-neration 5", "artist": "gya9", "soundtrack": "Technical Difficulties", "site": "https://youtu.be/yuFBlZbPdLI"},
+	{"album": "J-neration 6", "artist": "gya9", "soundtrack": "Kidding me!?", "site": "https://youtu.be/X-68zHxzdfs"},
+	{"album": "J-neration 7", "artist": "gya9", "soundtrack": "Kijimuna", "site": "https://j-neration.bandcamp.com/track/kijimuna"},
+	{"album": "J-neration 8", "artist": "gya9", "soundtrack": "Roar", "site": "https://j-neration.bandcamp.com/track/roar"},
+	{"album": "J-neration 9", "artist": "gya9", "soundtrack": "Lakad Mataaag!", "site": "https://youtu.be/P5l-hI1meMQ"},
+	{"album": "J-neration 1", "artist": "hurce", "soundtrack": "Short Circuit", "site": "https://youtu.be/0s1qtfAry7I"},
+	{"album": "J-neration 2", "artist": "hurce", "soundtrack": "Shepherd N", "site": "https://j-neration.bandcamp.com/track/shepherd-n"},
+	{"album": "J-neration 3", "artist": "hurce", "soundtrack": "Doline", "site": "https://youtu.be/QumGTQMr3bI"},
+	{"album": "J-neration 5", "artist": "hurce", "soundtrack": "Glory Sunshine", "site": "https://youtu.be/plGA7RRdOfc"},
+	{"album": "J-neration 6", "artist": "hurce", "soundtrack": "Statice", "site": "https://j-neration.bandcamp.com/track/statice"},
+	{"album": "J-neration 7", "artist": "hurce", "soundtrack": "Liliac", "site": "https://j-neration.bandcamp.com/track/liliac"},
+	{"album": "J-neration 9", "artist": "hurce", "soundtrack": "Light Up", "site": "https://youtu.be/Ny6h4U6P-2g"},
+	{"album": "J-neration 1", "artist": "Le Dos-on", "soundtrack": "Nu Clear", "site": "https://youtu.be/8LRYeplsZzk"},
+	{"album": "J-neration 4", "artist": "Moomei", "soundtrack": "Skyrocket", "site": "https://youtu.be/NOUSEhxb4iw"},
+	{"album": "J-neration 5", "artist": "Moomei", "soundtrack": "CYBRLXND", "site": "https://youtu.be/sXPmaOWEKsM"},
+	{"album": "J-neration 6", "artist": "Moomei", "soundtrack": "Start Again", "site": "https://youtu.be/MKpjM474QLE"},
+	{"album": "J-neration Gap 1", "artist": "Moomei", "soundtrack": "Brassfire", "site": "https://youtu.be/yq8eq885BuI"},
+	{"album": "J-neration 3", "artist": "nanobii", "soundtrack": "Hyper Drive", "site": "https://youtu.be/o_voTud0nhQ"},
+	{"album": "J-neration 4", "artist": "nanobii", "soundtrack": "Popsicle Beach", "site": "https://youtu.be/xiZpYdmaThM"},
+	{"album": "J-neration 5", "artist": "nanobii", "soundtrack": "RAVE, OK!", "site": "https://youtu.be/So1hJXVoAFw"},
+	{"album": "J-neration 6", "artist": "nanobii", "soundtrack": "UP & DOWN", "site": "https://youtu.be/-NhMW30sXjQ"},
+	{"album": "J-neration Gap 2", "artist": "nanobii", "soundtrack": "In My Fantasy", "site": "https://youtu.be/lZSYp9_xfbY"},
+	{"album": "J-neration 1", "artist": "Nizikawa", "soundtrack": "Jailbreak", "site": "https://youtu.be/URvSRID0TBk"},
+	{"album": "J-neration 2", "artist": "Nizikawa", "soundtrack": "SCREEEEEAM!", "site": "https://youtu.be/kT0DcbIfZhc"},
+	{"album": "J-neration 3", "artist": "Nizikawa", "soundtrack": "Utopia", "site": "https://youtu.be/ykDXOWLtu0o"},
+	{"album": "J-neration 4", "artist": "Nizikawa", "soundtrack": "Error Of Fortune", "site": "https://youtu.be/TCuS5phoKp4"},
+	{"album": "J-neration 5", "artist": "Nizikawa", "soundtrack": "Rule The Nation", "site": "https://j-neration.bandcamp.com/track/rule-the-nation"},
+	{"album": "J-neration 7", "artist": "Nizikawa", "soundtrack": "Limitbreak", "site": "https://youtu.be/ZccrPpKGKCc"},
+	{"album": "J-neration 8", "artist": "Nizikawa", "soundtrack": "Star Splash Saber", "site": "https://youtu.be/oJzojURD3Mc"},
+	{"album": "J-neration Gap 1", "artist": "Nizikawa", "soundtrack": "We Don't Need The Bass, Drop!!", "site": "https://j-neration.bandcamp.com/track/we-dont-need-the-bass-drop"},
+	{"album": "J-neration Gap 2", "artist": "Nizikawa", "soundtrack": "Peaky Jam", "site": "https://youtu.be/i4D1qMPLCm4"},
+	{"album": "J-neration Gap 1", "artist": "nora2r", "soundtrack": "Barbon House", "site": "https://youtu.be/LkVMzk0Hqzo"},
+	{"album": "J-neration 2", "artist": "sanaas", "soundtrack": "Polestar", "site": "https://youtu.be/2mwHcYffXAg"},
+	{"album": "J-neration 3", "artist": "sanaas", "soundtrack": "Reinforce", "site": "https://youtu.be/By3_kMYlNgI"},
+	{"album": "J-neration 4", "artist": "sanaas", "soundtrack": "Aeolis", "site": "https://youtu.be/1E_cKG_6oMs"},
+	{"album": "J-neration 6", "artist": "sanaas", "soundtrack": "(N)ever Goodbye", "site": "https://youtu.be/2Dk6cQx0yd4"},
+	{"album": "J-neration 7", "artist": "sanaas", "soundtrack": "RAVE INTO THE NIGHT", "site": "https://youtu.be/m-cuoUvGwqc"},
+	{"album": "J-neration 8", "artist": "sanaas", "soundtrack": "Away", "site": "https://youtu.be/rcUScplMDUo"},
+	{"album": "J-neration Gap 1", "artist": "sanaas", "soundtrack": "Penetrator", "site": "https://j-neration.bandcamp.com/track/penetrator"},
+	{"album": "J-neration Gap 2", "artist": "sanaas", "soundtrack": "You Make Me Feel", "site": "https://youtu.be/1kijFjFexKw"},
+	{"album": "J-neration 2", "artist": "Seventhrun", "soundtrack": "Chasing The Sun", "site": "https://youtu.be/vmCe7DoJVXg"},
+	{"album": "J-neration 3", "artist": "Seventhrun", "soundtrack": "Siren", "site": "https://youtu.be/XeiCCySkRIQ"},
+	{"album": "J-neration 4", "artist": "Seventhrun", "soundtrack": "GroundShaker", "site": "https://youtu.be/m4eXdE5PD20"},
+	{"album": "J-neration 5", "artist": "Seventhrun", "soundtrack": "Sakura Dreams", "site": "https://youtu.be/Ddkt5rULaF4"},
+	{"album": "J-neration 6", "artist": "Seventhrun", "soundtrack": "Might is Right", "site": "https://youtu.be/2ntJmdc03Rk"},
+	{"album": "J-neration 7", "artist": "Seventhrun", "soundtrack": "Nova Blaster", "site": "https://youtu.be/3HG_4k3nKA0"},
+	{"album": "J-neration 8", "artist": "Seventhrun", "soundtrack": "Hurricane", "site": "https://youtu.be/zjk_3lJSsA0"},
+	{"album": "J-neration 9", "artist": "Seventhrun", "soundtrack": "Raggafunk", "site": "https://youtu.be/PUu4y3BvVA8"},
+	{"album": "J-neration Gap 1", "artist": "Seventhrun", "soundtrack": "Soldier Dance", "site": "https://youtu.be/Q3Mvmm9Z3MI"},
+	{"album": "J-neration Gap 2", "artist": "Seventhrun", "soundtrack": "Decepticon", "site": "https://youtu.be/kFdGArkMuPs"},
+	{"album": "J-neration 8", "artist": "Soleily", "soundtrack": "Fulminating Silver", "site": "https://j-neration.bandcamp.com/track/fulminating-silver"},
+	{"album": "J-neration 9", "artist": "Soleily", "soundtrack": "Just Come Back", "site": "https://youtu.be/OZiPLBNTTVE"},
+	{"album": "J-neration 4", "artist": "Srav3R", "soundtrack": "Disco Light", "site": "https://youtu.be/Q9T3zbm911I"},
+	{"album": "J-neration 5", "artist": "Srav3R", "soundtrack": "Reworld", "site": "https://youtu.be/IuQYtC71a9M"},
+	{"album": "J-neration 6", "artist": "Srav3R", "soundtrack": "Unknown Garden", "site": "https://j-neration.bandcamp.com/track/unknown-garden"},
+	{"album": "J-neration 7", "artist": "Srav3R", "soundtrack": "Just Now", "site": "https://j-neration.bandcamp.com/track/just-now"},
+	{"album": "J-neration 8", "artist": "Srav3R", "soundtrack": "Activizm", "site": "https://j-neration.bandcamp.com/track/activizm"},
+	{"album": "J-neration 9", "artist": "Srav3R", "soundtrack": "Fall Night", "site": "https://youtu.be/PTI4xmSb94E"},
+	{"album": "J-neration Gap 1", "artist": "Srav3R", "soundtrack": "Strange Horizon", "site": "https://youtu.be/UBnx1Qc77EA"},
+	{"album": "J-neration Gap 2", "artist": "Srav3R", "soundtrack": "J-ALARM", "site": "https://j-neration.bandcamp.com/track/j-alarm"},
+	{"album": "J-neration 9", "artist": "Street", "soundtrack": "Victory Stage!!", "site": "https://youtu.be/gTAi545s2RQ"},
+	{"album": "J-neration 1", "artist": "Synagi", "soundtrack": "I'll Show U", "site": "https://youtu.be/1-vwrDdsCoU"},
+	{"album": "J-neration 2", "artist": "Synagi", "soundtrack": "Ikutose", "site": "https://youtu.be/zWBC8TEuv50"},
+	{"album": "J-neration 3", "artist": "Synagi", "soundtrack": "Tokoshie", "site": "https://youtu.be/lnib__1UAko"},
+	{"album": "J-neration 4", "artist": "Synagi", "soundtrack": "Fading", "site": "https://youtu.be/uEe-6mX53lw"},
+	{"album": "J-neration 5", "artist": "Synagi", "soundtrack": "Kotonoha", "site": "https://youtu.be/iSfU-a-Ta9I"},
+	{"album": "J-neration 6", "artist": "Synagi", "soundtrack": "E.D.E.N.", "site": "https://youtu.be/nb4yFK_rJL4"},
+	{"album": "J-neration 7", "artist": "Synagi", "soundtrack": "Namida", "site": "https://youtu.be/0JYRz-cSmic"},
+	{"album": "J-neration 8", "artist": "Synagi", "soundtrack": "Tokyo Hitotose", "site": "https://j-neration.bandcamp.com/track/tokyo-hitotose"},
+	{"album": "J-neration 9", "artist": "Synagi", "soundtrack": "Setsuna.", "site": "https://youtu.be/-7McZq5_vW0"},
+	{"album": "J-neration Gap 1", "artist": "Synagi", "soundtrack": "Kick Da Floor", "site": "https://youtu.be/WSVHhJgRvu8"},
+	{"album": "J-neration Gap 2", "artist": "Synagi", "soundtrack": "Crimson-Eye", "site": "https://youtu.be/tqKBVLiRK2o"},
+	{"album": "J-neration 6", "artist": "TANUKI", "soundtrack": "Wanderer", "site": "https://youtu.be/h14Z5R1W3Xw"},
+	{"album": "J-neration 1", "artist": "you", "soundtrack": "Mermaid Wink", "site": "https://youtu.be/MVzLEMIHRcE"},
+	{"album": "J-neration 2", "artist": "you", "soundtrack": "Butterfly Lily", "site": "https://j-neration.bandcamp.com/track/butterfly-lily"},
+	{"album": "J-neration 2", "artist": "you vs ETIA", "soundtrack": "Shang a Lang feat. nayuta", "site": "https://youtu.be/k56884fYLYU"},
+	{"album": "J-neration 3", "artist": "you", "soundtrack": "Let's Jump! feat. nayuta", "site": "https://youtu.be/N5TMRpbEzps"},
+	{"album": "J-neration 4", "artist": "you", "soundtrack": "Here We Go! feat. nayuta", "site": "https://youtu.be/Bub8G8clvB0"},
+	{"album": "J-neration 5", "artist": "you", "soundtrack": "Dawn feat. nayuta", "site": "https://youtu.be/aAXRW5NEleY"},
+	{"album": "J-neration 6", "artist": "you", "soundtrack": "HELLO! feat. nayuta", "site": "https://youtu.be/y103UKYS2-g"},
+	{"album": "J-neration 7", "artist": "you", "soundtrack": "Compass feat. nayuta", "site": "https://youtu.be/lp0GIzW2SBk"},
+	{"album": "J-neration 8", "artist": "you", "soundtrack": "Color feat. nayuta", "site": "https://youtu.be/wjvXT8guXZQ"},
+	{"album": "J-neration 9", "artist": "you", "soundtrack": "Comet feat. nayuta", "site": "https://youtu.be/7uWJvtvumBE"},
+	{"album": "J-neration Gap 1", "artist": "you", "soundtrack": "Honey Pallet", "site": "https://youtu.be/Ajljq8PNwEk"},
+	{"album": "J-neration Gap 2", "artist": "you", "soundtrack": "Rockin Girl", "site": "https://j-neration.bandcamp.com/track/rockin-girl"}
+];
+
+function searchsong() {
+	var songstore = document.getElementById ("findsong");
+	var	typevalue = songstore.value.toLowerCase();
+	var tracktable = document.getElementById("info");
+	cleartable();
+	for (var i = 0; i < database.length; i++) {
+		var entry = database[i];
+		if (entry.soundtrack.toLowerCase().includes(typevalue)) {
+			printsong(entry);
+		}
+	}
+	return false;
+}
+
+function searchartist() {
+	var artiststore = document.getElementById ("findartist");
+	var	typevalue = artiststore.value.toLowerCase();
+	var tracktable = document.getElementById("info");
+	cleartable();
+	for (var i = 0; i < database.length; i++) {
+		var entry = database[i];
+		if (entry.artist.toLowerCase().includes(typevalue)) {
+			printsong(entry);
+		}
+	}
+	return false;
+}
+
+function searchalbum() {
+	var albumstore = document.getElementById ("findalbum");
+	var	typevalue = albumstore.value.toLowerCase();
+	var tracktable = document.getElementById("info");
+	cleartable();
+	for (var i = 0; i < database.length; i++) {
+		var entry = database[i];
+		if (entry.album.toLowerCase().includes(typevalue)) {
+			printsong(entry);
+		}
+	}
+	return false;
+}
+function printsong(entry) {
+	var songstore = document.getElementById ("findsong");
+	var	typevalue = songstore.value.toLowerCase();
+	var tracktable = document.getElementById("info");
+	console.log(entry);
+	var trackrow = document.createElement("tr");
+	var artistCell = document.createElement("td");
+	var albumCell = document.createElement("td");
+	var soundtrackCell = document.createElement("td");
+	artistCell.innerText = entry.artist;
+	albumCell.innerText = entry.album;
+	trackrow.appendChild(artistCell);
+	trackrow.appendChild(albumCell);
+	trackrow.appendChild(soundtrackCell);
+	tracktable.appendChild(trackrow);
+	var linkInsert = document.createElement("a");
+	linkInsert.href = entry.site;
+	linkInsert.target = "_blank";
+	linkInsert.innerText = entry.soundtrack;
+	soundtrackCell.appendChild(linkInsert);
+}
+
+function cleartable() {
+	var tracktable = document.getElementById("info");
+	while(tracktable.childNodes.length > 2){
+		tracktable.removeChild(tracktable.childNodes[2])
+	}
+}
